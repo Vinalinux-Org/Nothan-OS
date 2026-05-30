0dec417ee0dd72d1902f7626ac240dbbe6e9475a
haidoan2098 <haidoan2098@gmail.com>
2026-05-30 17:24:07 +0700

    Feat(kernel): add exception vectors and mode stacks
    - Vector table + trampolines for all 7 exception types
    - C handler stubs (und, svc, pabt, dabt, irq, fiq)
    - Mode stacks (FIQ, IRQ, ABT, UND, SVC) before and after MMU
    - VBAR set at phys before MMU, at VA after MMU enable

---
+++ nothan-kernel/arch/arm/kernel/traps.c
@@ -0,0 +1,37 @@
+#include <nothan/types.h>
+
+void und_handler(unsigned int spsr)
+{
+	while (1)
+		;
+}
+
+void svc_handler(unsigned int spsr)
+{
+	while (1)
+		;
+}
+
+void pabt_handler(unsigned int spsr)
+{
+	while (1)
+		;
+}
+
+void dabt_handler(unsigned int spsr)
+{
+	while (1)
+		;
+}
+
+void irq_handler(unsigned int spsr)
+{
+	while (1)
+		;
+}
+
+void fiq_handler(unsigned int spsr)
+{
+	while (1)
+		;
+}
