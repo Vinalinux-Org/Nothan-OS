23322387371e56fe441cedbae70101a7e825b936
haidoan2098 <haidoan2098@gmail.com>
2026-06-29 14:24:22 +0700

    Feat(libc): add minimal vsnprintf, string helpers, and fixed-width types
    - vsnprintf/snprintf/printf supporting %d %u %x %c %s %.*s %04x
    - strchr, strstr, strncpy, memmove, atoi
    - uint8_t/uint16_t/uint32_t/ssize_t (no newlib/stdint.h)
    - All static inline, no heap, verified on host + arm-none-eabi

--- userspace/lib/string.h
+++ userspace/lib/string.h
@@ -1,7 +1,9 @@
 #ifndef _NOTHAN_STRING_H
 #define _NOTHAN_STRING_H
 
-typedef unsigned long size_t;
+#ifndef size_t
+#define size_t unsigned long
+#endif
 
 static inline size_t strlen(const char *s)
 {
@@ -36,6 +38,59 @@ static inline void *memset(void *dst, int c, size_t n)
 	char *d = dst;
 	while (n--) *d++ = (char)c;
 	return dst;
+}
+
+static inline void *memmove(void *dst, const void *src, size_t n)
+{
+	char *d = dst;
+	const char *s = src;
+	if (d < s) {
+		while (n--) *d++ = *s++;
+	} else {
+		d += n; s += n;
+		while (n--) *--d = *--s;
+	}
+	return dst;
+}
+
+static inline char *strchr(const char *s, int c)
+{
+	for (; *s; s++)
+		if (*s == (char)c)
+			return (char *)s;
+	return (c == 0) ? (char *)s : (char *)0;
+}
+
+static inline char *strstr(const char *h, const char *n)
+{
+	if (!*n)
+		return (char *)h;
+	for (; *h; h++) {
+		const char *a = h, *b = n;
+		while (*a && *b && *a == *b) { a++; b++; }
+		if (!*b)
+			return (char *)h;
+	}
+	return (char *)0;
+}
+
+static inline char *strncpy(char *dst, const char *src, size_t n)
+{
+	size_t i = 0;
+	for (; i < n && src[i]; i++) dst[i] = src[i];
+	for (; i < n; i++) dst[i] = '\0';
+	return dst;
+}
+
+static inline int atoi(const char *s)
+{
+	int sign = 1, v = 0;
+	while (*s == ' ' || *s == '\t') s++;
+	if (*s == '-') { sign = -1; s++; }
+	else if (*s == '+') s++;
+	while (*s >= '0' && *s <= '9')
+		v = v * 10 + (*s++ - '0');
+	return sign * v;
 }
 
 #endif /* _NOTHAN_STRING_H */
